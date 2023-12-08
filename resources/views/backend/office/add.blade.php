525483dc239e522fc20e791faf1c3879ca3cdc27
YulinarNR <yulinarnurrahma@gmail.com>
2023-12-08 15:33:06 +0700

    [Designing] Read Add data office

--- resources/views/backend/office/add.blade.php
+++ resources/views/backend/office/add.blade.php
@@ -12,27 +12,26 @@
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Form Data Office</h3>
+                    <h3><i class="bi bi-file-earmark-plus"></i> Form Data Office</h3>
                     <p class="text-subtitle text-muted">Menambah Data Kantor</p>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Data Office</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Form Data Office</li>
+                            <li class="breadcrumb-item"><a href="{{ route('backend.office') }}"><i class="bi bi-house-door"></i> Data Office</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><i class="bi bi-file-earmark-plus"></i> Form Data Office</li>
                         </ol>
                     </nav>
                 </div>
             </div>
         </div>
 
-        <!-- // Basic multiple Column Form section start -->
         <section id="multiple-column-form">
             <div class="row match-height">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Add Data</h4>
+                            <h4 class="card-title"><i class="bi bi-plus-circle"></i> Add Data</h4>
                         </div>
                         <div class="card-content">
                             <div class="card-body">
@@ -48,7 +47,6 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        
                                         <div class="col-md-6 col-12">
                                             <div class="form-group">
                                                 <label for="region_id">Daerah</label>
@@ -66,19 +64,17 @@
                                             </div>
                                         </div>
                                         <div class="col-12 d-flex justify-content-end">
-                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
-                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                            <button type="submit" class="btn btn-primary me-1 mb-1"><i class="bi bi-check"></i> Submit</button>
+                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1"><i class="bi bi-x"></i> Reset</button>
                                         </div>
                                     </div>
                                 </form>
-                                                       
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        <!-- // Basic multiple Column Form section end -->
     </div>
 </div>
 @endsection
--- resources/views/backend/office/index.blade.php
+++ resources/views/backend/office/index.blade.php
@@ -2,71 +2,73 @@
 
 @section('content')
 <div id="main">
-  <header class="mb-3">
-      <a href="#" class="burger-btn d-block d-xl-none">
-          <i class="bi bi-justify fs-3"></i>
-      </a>
-  </header>
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
 
-  <div class="page-heading">
-      <div class="page-title">
-          <div class="row">
-              <div class="col-12 col-md-6 order-md-1 order-last">
-                  <h3>Data Office</h3>
-              </div>
-              <div class="col-12 col-md-6 order-md-2 order-first">
-                  <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                      <ol class="breadcrumb">
-                          <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                          <li class="breadcrumb-item active" aria-current="page">DataTable</li>
-                      </ol>
-                  </nav>
-              </div>
-          </div>
-      </div>
-      <section class="section">
-          <div class="card">
-            @if(session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Data Office</h3>
                 </div>
-            @endif
-            <div class="card-header d-flex justify-content-between align-items-center">
-              Daftar Data Kantor
-              <a class="btn btn-primary" href="{{ route('backend.officeAdd') }}">Add</a>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('backend.dashboard') }}"><i class="bi bi-house-door"></i> Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><i class="bi bi-table"></i> DataTable</li>
+                        </ol>
+                    </nav>
+                </div>
             </div>
-              <div class="card-body">
-                  <table class="table table-striped" id="table1">
-                    <thead>
-                      <tr>
-                        <th>Jenis Kantor</th>
-                        <th>Daerah</th>
-                        <th>Alamat</th>
-                        <th>Aksi</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      @foreach ($office as $office_item)
-                          <tr>
-                            <td>{{ $office_item->office_type }}</td>
-                            <td>{{ $office_item->region ? $office_item->region->city_name : 'N/A' }}</td>
-                            <td>{{ $office_item->address }}</td>
-                            <td>
-                              <a href="{{ route('region.edit', $office_item->id) }}" class="btn btn-primary">Edit</a>
-                              <form action="{{ route('region.destroy', $office_item->id) }}" method="POST" style="display:inline;">
-                                  @csrf
-                                  @method('DELETE')
-                                  <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
-                              </form>
-                          </td>
-                          </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
-              </div>
-          </div>
+        </div>
 
-      </section>
-  </div>
+        <section class="section">
+            <div class="card">
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                <div class="card-header d-flex justify-content-between align-items-center">
+                  <span><i class="bi bi-house-door"></i> Daftar Data Kantor</span>
+                    <a class="btn btn-primary" href="{{ route('backend.officeAdd') }}"><i class="bi bi-plus"></i> Add</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>Jenis Kantor</th>
+                                    <th>Daerah</th>
+                                    <th>Alamat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($office as $office_item)
+                                    <tr>
+                                        <td>{{ $office_item->office_type }}</td>
+                                        <td>{{ $office_item->region ? $office_item->region->city_name : 'N/A' }}</td>
+                                        <td>{{ $office_item->address }}</td>
+                                        <td>
+                                            <a href="{{ route('region.edit', $office_item->id) }}" class="btn btn-primary btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"><i class="bi bi-pencil"></i></a>
+                                            <form action="{{ route('region.destroy', $office_item->id) }}" method="POST" style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')"><i class="bi bi-trash"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </div>
 @endsection
